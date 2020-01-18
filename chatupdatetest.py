d8a1843fcf776b28aa470c3c27724a9cab09d952
Nightarcher3677 <50910438+Nightarcher3677@users.noreply.github.com>
2020-01-18 13:24:09 +1100

    socket google connection converted from python 2

--- chatupdatetest.py
+++ chatupdatetest.py
@@ -3,9 +3,9 @@ import sys
 
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print "Socket successfully created"
+    print("Socket successfully created")
 except socket.error as err:
-    print "socket creation failed with error %s" %(err)
+    print("socket creation failed with error %s") %(err)
 
 # default port for socket
 port = 80
@@ -15,11 +15,11 @@ try:
 except socket.gaierror:
 
     # this means could not resolve the host
-    print "there was an error resolving the host"
+    print("there was an error resolving the host")
     sys.exit()
 
 # connecting to the server
 s.connect((host_ip, port))
 
-print "the socket has successfully connected to google \
-on port == %s" %(host_ip) 
+print("the socket has successfully connected to google \
+on port yg")
