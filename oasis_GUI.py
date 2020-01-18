ff2b343f93e39e47d5ea88133b4a76da8c21db31
Nightarcher3677 <50910438+Nightarcher3677@users.noreply.github.com>
2020-01-19 10:48:58 +1100

    oasis_gui update

--- oasis_GUI.py
+++ oasis_GUI.py
@@ -1,1 +1,14 @@
+import Tkinter
+top = Tkinter.Tk()
+# Code to add widgets will go here...
 
+L1 = Label(top, text="Username")
+L1.pack( side = LEFT)
+E1 = Entry(top, bd =5)
+E1.pack(side = RIGHT)
+L1 = Label(top, text="Password")
+L1.pack( side = LEFT)
+E1 = Entry(top, bd =5)
+E1.pack(side = RIGHT)
+
+top.mainloop()
