475657594a3da51171fe74b7376f293d34bdc330
Nightarcher3677 <50910438+Nightarcher3677@users.noreply.github.com>
2020-01-18 13:55:31 +1100

    socket not working, need assistance

--- chatupdatetest.py
+++ chatupdatetest.py
@@ -1,25 +1,36 @@
+# first of all import the socket library
 import socket
-import sys
 
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("Socket successfully created")
-except socket.error as err:
-    print("socket creation failed with error %s") %(err)
+# next create a socket object
+s = socket.socket()
+print("Socket successfully created")
 
-# default port for socket
-port = 80
+# reserve a port on your computer in our
+# case it is 12345 but it can be anything
+port = 12345
 
-try:
-    host_ip = socket.gethostbyname('www.google.com')
-except socket.gaierror:
+# Next bind to the port
+# we have not typed any ip in the ip field
+# instead we have inputted an empty string
+# this makes the server listen to requests
+# coming from other computers on the network
+s.bind(('', port))
+print("socket binded to ")
+print(port)
+# put the socket into listening mode
+s.listen(5)
+print("socket is listening")
 
-    # this means could not resolve the host
-    print("there was an error resolving the host")
-    sys.exit()
+# a forever loop until we interrupt it or
+# an error occurs
+while True:
 
-# connecting to the server
-s.connect((host_ip, port))
+   # Establish connection with client.
+   c, addr = s.accept()
+   print('Got connection from', addr)
 
-print("the socket has successfully connected to google \
-on port " + host_ip)
+   # send a thank you message to the client.
+   c.send('Thank you for connecting')
+
+   # Close the connection with the client
+   c.close()
