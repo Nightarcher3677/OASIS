8c11d8b34cf735c637942e1d2baf7fb78c71d0ed
Nightarcher3677 <50910438+Nightarcher3677@users.noreply.github.com>
2020-01-19 11:42:14 +1100

    made command for database, created database.py

--- oasis_GUI.py
+++ oasis_GUI.py
@@ -1,6 +1,12 @@
-from tkinter import *
 import os
-
+import logging
+import logging.handlers
+import subprocess
+import time, sys
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+import tkinter
+import ssl
 win = Tk()
 win.title("OASIS")
 win.resizable(width=False, height=False)
@@ -17,6 +23,9 @@ systeminfoimg = PhotoImage(file="chat_button.png")
 systeminfo = Button(win, text="SYSTEMINFO", image=systeminfoimg, width=150, height=67,borderwidth=0, highlightthickness= 0, command= lambda: os.popen("chat.py"))
 systeminfo.place(x=35, y=150)
 
+adminimg = PhotoImage(file="database.png")
+admin = Button(win, text="TASKMGR", width=150, height=67, highlightthickness= 0, borderwidth=0, image=adminimg, command= lambda: os.popen("database.py"))
+admin.place(x=205, y=150)
 
 
 
