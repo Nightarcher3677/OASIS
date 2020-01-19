3084bc0e6c3b1f6dc3b3da2e8d2842db69317670
Nightarcher3677 <50910438+Nightarcher3677@users.noreply.github.com>
2020-01-19 11:21:41 +1100

    fixed title

--- oasis_GUI.py
+++ oasis_GUI.py
@@ -2,12 +2,12 @@ from tkinter import *
 import os
 
 win = Tk()
-win.title("Hades")
+win.title("OASIS")
 win.resizable(width=False, height=False)
 win.geometry("900x600")
 win.configure(background="Black")
 
-fondoimg = PhotoImage(file="logo.png")
+fondoimg = PhotoImage(file="GUI_Background.png")
 fondoshow = Label(win, text="", image=fondoimg)
 fondoshow.pack()
 
