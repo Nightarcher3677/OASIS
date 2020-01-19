2e1f7ca0ebc339db3d6f8721958b0759e8f56a7c
Nightarcher3677 <50910438+Nightarcher3677@users.noreply.github.com>
2020-01-20 10:01:36 +1100

    chat update

--- chat_pick.py
+++ chat_pick.py
@@ -10,9 +10,9 @@ win.configure(background="Black")
 win.iconbitmap("applogo.ico")
 
 systeminfoimg = PhotoImage(file="chat_client.png")
-systeminfo = Button(win, text="SYSTEMINFO", image=systeminfoimg, width=150, height=67,borderwidth=0, highlightthickness= 0, command= lambda: os.popen("chat.py"))
+systeminfo = Button(win, text="SYSTEMINFO", image=systeminfoimg, width=150, height=67,borderwidth=0, highlightthickness= 0, command= lambda: os.popen("chat_client.py"))
 systeminfo.place(x=35, y=150)
 
 createfileimg = PhotoImage(file="chat_server.png")
-createfile = Button(win, text="createfile", image=createfileimg, width=150, height=67,borderwidth=0, highlightthickness= 0, command= lambda: os.popen("createfile.py"))
+createfile = Button(win, text="createfile", image=createfileimg, width=150, height=67,borderwidth=0, highlightthickness= 0, command= lambda: os.popen("chat.py"))
 createfile.place(x=545, y=150)
