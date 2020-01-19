9dbd6ddc025cc27b7c642ebec7b074c675305e54
Nightarcher3677 <50910438+Nightarcher3677@users.noreply.github.com>
2020-01-20 10:15:04 +1100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='OASIS',
+    version='V0.1',
+    packages=[''],
+    url='https://github.com/Nightarcher3677/OASIS',
+    license='GPL-3.0',
+    author='Nightarcher3677',
+    author_email='Nightarcher3677@gmai.com',
+    description='A python GUI that gives access to many functions.'
+)
